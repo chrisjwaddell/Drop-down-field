73365ef8123237f7409cc675f826aaaf7109cfb8
Chris Waddell <yesitschris@hotmail.com>
2023-05-19 12:03:58 +1000

    Minor update

--- src/scripts/dropdown-field.js
+++ src/scripts/dropdown-field.js
@@ -667,7 +667,7 @@ export default function DropdownField(
 	}
 
 	function onKeyDownInput(e) {
-		if (keyCodes[event.keyCode] === "tab") {
+		if (keyCodes[e.keyCode] === "tab") {
 			if (elAutocomplete) {
 				if (elAutocomplete.classList.contains("isvisible")) {
 					elInput.value = elAutocomplete.textContent
