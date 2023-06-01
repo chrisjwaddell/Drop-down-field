693a13b6766d3e248cbcd524df3625d832740cf3
Chris Waddell <yesitschris@hotmail.com>
2023-06-01 19:31:20 +1000

    [Fix] onChange activated when pressing Delete key and some other special keys

--- src/scripts/dropdown-field.js
+++ src/scripts/dropdown-field.js
@@ -843,6 +843,7 @@ export default function DropdownField(
 							if (elAutocomplete)
 								elAutocomplete.classList.remove("isvisible")
 						}
+						if (settings.onChange) settings.onChange()
 					} else if (results.length === 0) {
 						matches = []
 						elUL.innerHTML = ""
@@ -850,6 +851,7 @@ export default function DropdownField(
 
 						if (elAutocomplete)
 							elAutocomplete.classList.remove("isvisible")
+						if (settings.onChange) settings.onChange()
 					} else {
 						// Letters have been typed, they are shown as bold
 						matches = results.map((cv) =>
